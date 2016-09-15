d983eeafe1d20df3df89a43c30422fd84c8791de
colemonnahan <monnahc@uw.edu>
2016-09-15 16:09:43 -0700

    Rework experimental analysis

--- run_experimental.R
+++ run_experimental.R
@@ -1,28 +1,27 @@
+source("startup.R")
+
 ## load and process the empirical data
 hs <- read.csv("data/hs_data.csv")
-hs <- subset(hs, used=="Y")             #
+hs <- droplevels(subset(hs, used=="Y" & lbs>0))
 hs$hooks <- with(hs, hooks.per.skate*skates)
-hs <- within(hs, {catch.per.hook=lbs/hooks; number.per.hook=number/hooks})
-## A unique identifier for each row, for merging later
-hs$rowID <- 1:nrow(hs)
+hs$catch <- hs$lbs
 ## Normalize days
 hs$date <- lubridate::mdy(as.character(hs$date))
 hs <- ddply(hs, .(group), mutate,
               daynumber=as.numeric(difftime(date,min(date), units="days")))
 hs <- hs[with(hs, order(group, date)),]
 hs$group <- as.numeric(hs$group)
-## The dependent variable (catch.per.hook for now) on log scale since it
-## needs to be positive. For now adding .1 but should revisit later
 
-data.hs <-
-    list(log_yobs=log(hs$catch.per.hook+.1), group=hs$group-1,
-         Nobs=nrow(hs), Ngroup=length(unique(hs$group)), day=hs$daynumber,
-         spacing=hs$spacing)
+## Prepare TMB inputs
+Data <-
+  list(n_i=nrow(hs), n_s=length(unique(hs$group)),
+       catch_i=hs$catch, hooks_i=hs$hooks, day_i=hs$daynumber,
+       spacing_i=hs$spacing, site_i=hs$group-1)
 Params <-
-    list(logcpue_mean=0, logcpue_sd=1, sigma_obs_mean=-.5, sigma_obs_sd=.5,
-         gamma=0, beta=.1, lambda=1, logcpue=rep(0, len=data.hs$Ngroup), logsigma_obs=rep(0,
-                                                        len=data.hs$Ngroup))
-Map <- list(lambda=factor(NA))
+    list(eta_mean=1, eta_sd=1, sigma_mean=-.5, sigma_sd=.5,
+         gamma=0.1, beta=.5, lambda=1,
+         eta_s=rep(1, len=Data$n_s), sigma_s=rep(1,len=Data$n_s))
+Map <- list(lambda=factor(NA))          # can't estimate this so fix at 1
 Version <- "models/empirical_spacing"
 compile( paste0(Version,".cpp") )
 dyn.load( dynlib(Version) )
@@ -32,19 +31,22 @@ upper <- rep(Inf,  len=length(unlist(Params)))
 names(upper) <- names(lower) <- names(unlist(Params))
 lower['gamma'] <- 0; upper['gamma'] <- 1
 lower['lambda'] <- 0; upper['lambda'] <- 5
-lower['logcpue_sd'] <- 0; upper['logcpue_sd'] <- Inf
-lower['sigma_obs_sd'] <- 0; upper['sigma_obs_sd'] <- Inf
-Obj <- MakeADFun(data=data.hs, parameters=Params,
-                 random=c('logsigma_obs', 'logcpue'), map=Map)
+lower['eta_sd'] <- 0; upper['eta_sd'] <- Inf
+lower['sigma_sd'] <- 0; upper['sigma_sd'] <- Inf
+
+## Make object and optimize
+Obj <- MakeADFun(data=Data, parameters=Params,
+                 random=c('eta_s', 'sigma_s'), map=Map)
 Obj$env$beSilent()
 Opt <- nlminb( start=Obj$par, objective=Obj$fn, gradient=Obj$gr,
                control=list(trace=10, eval.max=1e4, iter.max=1e4),
                             lower=lower, upper=upper)
 temp <- sdreport(Obj)
 xx <- data.frame(par=names(temp$value), value=temp$value, sd=temp$sd)
-uncertainty.df <- subset(xx, par=='ehook')
+uncertainty.df <- subset(xx, par=='hook_power')
 uncertainty.df$spacing <- 1:nrow(uncertainty.df)
-sd.df <- subset(xx, par!= 'ehook')
+sd.df <- subset(xx, par!= 'hook_power')
+ggplot(uncertainty.df, aes(spacing, value, ymax=value+2*sd, ymin=value-2*sd)) + geom_pointrange()
 empirical.results <- list(Obj=Obj, Opt=Opt, sdreport=temp,
                           uncertainty.df=uncertainty.df, sd.df=sd.df)
 saveRDS(empirical.results, file='results/empirical.results.RDS')
